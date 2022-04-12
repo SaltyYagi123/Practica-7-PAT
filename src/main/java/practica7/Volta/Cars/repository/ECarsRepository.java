6e920eb1a85526e37ea76d8d69963f4819d0fa61
Yago Tobio Souto <201802168@alu.comillas.edu>
2022-04-12 17:37:21 +0000

    Repository Base Start

---
+++ src/main/java/practica7/Volta/Cars/repository/ECarsRepository.java
@@ -0,0 +1,5 @@
+package practica7.Volta.Cars.repository;
+import practica7.Volta.Cars.model.*;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ECarsRepository extends CrudRepository<ECarTable, Long>{}
