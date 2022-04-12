8140554da716d8c70dadd541257eab6ab9c98cc3
Yago Tobio Souto <201802168@alu.comillas.edu>
2022-04-12 16:18:39 +0000

    Repositorio Default y Manejo de Datos Inicial

---
+++ src/main/java/practica7/Volta/Cars/VoltaCarsApplication.java
@@ -0,0 +1,13 @@
+package practica7.Volta.Cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoltaCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoltaCarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practica7/Volta/Cars/VoltaCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package practica7.Volta.Cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoltaCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
