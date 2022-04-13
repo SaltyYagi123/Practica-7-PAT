753366bd3cacc7db026cd5b15c7161de53c619c0
Yago Tobio Souto <201802168@alu.comillas.edu>
2022-04-13 23:09:50 +0200

    I LIKE LITERALLY DON'T EVEN CARE ANYMORE

--- src/main/java/practica7/Volta/Cars/service/impl/VehicleServiceImplementation.java
+++ src/main/java/practica7/Volta/Cars/service/impl/VehicleServiceImplementation.java
@@ -1,37 +1,33 @@
 package practica7.Volta.Cars.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import practica7.Volta.Cars.repository.VehiclesRepository;
 import practica7.Volta.Cars.service.VehicleService;
-
+import practica7.Volta.Cars.service.dto.VehicleDTO;
 
 @Service
 public class VehicleServiceImplementation implements VehicleService {
-    @Autowired 
+    @Autowired
     private VehiclesRepository vehicleRepository;
 
     @Override
-    public List<VehicleDTO> retrieveVehicles()
-    {
+    public List<VehicleDTO> retrieveVehicles() {
         return vehicleRepository.findAll();
     }
 
     @Override
-    public List<VehicleDTO> retrieveVehiclesElectric(boolean electric)
-    {
-        if(electric == null)
-        {
-            return vehicleRepository.findAll();
-        }else{
+    public List<VehicleDTO> retrieveVehiclesElectric(boolean electric) {
             return vehicleRepository.retrieveVehiclesByElectric(electric);
-        }
     }
 
 }
 
-/*    List<VehicleDTO> getVehicles();
-
-    List<VehicleDTO> getVehiclesWithUsers();
-*/
+/*
+ * List<VehicleDTO> getVehicles();
+ * 
+ * List<VehicleDTO> getVehiclesWithUsers();
+ */
